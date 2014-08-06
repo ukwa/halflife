8b2596d5101eb80301b22b50172bb11b06a0d65a
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-08-06 06:59:29 +0100

    Updated to cope with new similarity measures.

--- tools/halflife/scan_graph.py
+++ tools/halflife/scan_graph.py
@@ -1,20 +1,27 @@
 import csv, sys, pprint
 import collections
 
-filename = 'halflife/sample-v2-3.tsv'
+filename =  sys.argv[1]
 counts = collections.defaultdict(lambda: collections.defaultdict(int))
 with open(filename, 'rb') as f:
     reader = csv.reader(f, delimiter="\t")
     for row in reader:
-        #print row
+        counts[row[0]]["TOTAL"] += 1
         counts[row[0]][row[3]] += 1
+        identical = row[6]
+        if identical == "True":
+            counts[row[0]]["IDENTICAL"] += 1
+        similarity = row[7]
+        if similarity != "None" and similarity != "" and int(similarity) > 20:
+            counts[row[0]]["SIMILAR"] += 1
+
 
 # Create the header line
-print("\t".join(['Year', 'GONE', 'ERROR', 'MISSING','MOVED','OK']))
+print("\t".join(['Year', 'GONE', 'ERROR', 'MISSING','MOVED','OK', 'IDENTICAL','SIMILAR','TOTAL']))
 for year in sorted(counts.keys()):
     # Now build up data
     line = year
-    for state in ['GONE', 'ERROR', 'MISSING','MOVED','OK']:
+    for state in ['GONE', 'ERROR', 'MISSING','MOVED','OK','IDENTICAL','SIMILAR','TOTAL']:
         line = line + "\t" + str(counts[year][state])
     print(line)
 
